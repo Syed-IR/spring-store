2e5b6d3782bfc14a9daaa7229f31a37d1fde0606
Syed Irtaza <syed.128@outlook.com>
2025-04-01 22:57:58 +0500

    Configuring Beans Using Code

---
+++ src/main/java/com/codewithmosh/store/AppConfig.java
@@ -0,0 +1,26 @@
+package com.codewithmosh.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Value("${payment-gateway}")
+    private String paymentMode;
+
+    @Bean
+    public PaymentService stripe() {
+        return new StripePaymentService();
+    }
+
+    @Bean
+    public PaymentService paypal() {
+        return new PaypalPaymentService();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderService(paymentMode.equals("stripe") ? stripe() : paypal());
+    }
+}
--- src/main/java/com/codewithmosh/store/OrderService.java
+++ src/main/java/com/codewithmosh/store/OrderService.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 // or @Component. @Service is just an alias for @component
-@Service
+//@Service
 public class OrderService {
     private final PaymentService paymentService;
 
     // @Autowired
     // public OrderService(PaymentService paymentService)
-    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
+//    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+    // Custom Bean configuration
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
     public void placeOrder() {
